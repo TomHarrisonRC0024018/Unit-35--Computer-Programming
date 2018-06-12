50f5b2de0e4e4d452e2d5f27eed7f9c4a0ebbdf9
Thomas Harrison <RC0024018@student.riversidecollege.ac.uk>
2018-06-12 13:28:38 +0100

    jh

--- Unit 35- Computer Programming/Form1.cs
+++ Unit 35- Computer Programming/Form1.cs
@@ -7,14 +7,70 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Unit_35__Computer_Programming
 {
     public partial class Form1 : Form
     {
+        class row
+        {
+            public double acceleration;
+            public double velocity;
+            public double altitude;
+            public double dAltitude;
+
+        
+        }
+
+        List<row> table = new List<row>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculateAltitude()
+        {
+            for (int i=1; 1 > table.Count; i++)
+            {
+                double dAltitude = table[i].velocity - table[i - 1].velocity;
+            }
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "csv files|*.csv";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                    {
+                        string line = sr.ReadLine();
+                        while (!sr.EndOfStream)
+                        {
+                            table.Add(new row());
+                            string[] r = sr.ReadLine().Split(',');
+                            table.Last().acceleration = double.Parse(r[0]);
+                            table.Last().velocity = double.Parse(r[1]);
+                        }
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " failed to open.");
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    MessageBox.Show(openFileDialog1.FileName + " is not in the required format");
+                }
+            }
         }
     }
 }
